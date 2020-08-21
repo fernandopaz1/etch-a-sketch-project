0dda3e6b086f8dfdef9c5dd5afff4fdb1d505c3c
Fernando Paz <ferpaz9292@gmail.com>
2020-08-21 17:08:28 -0300

    Error found: I should call addGrid before addEventListener

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -9,7 +9,24 @@ function addGridFile(cantColumnas){
 
 function addGrid(){
     document.getElementById("gridContainer").innerHTML +=  
-    "<div class='gridUnit'></div>";
+    '<div class="gridUnit">Hi</div>';
 }
 
-addGridFile(16);
+function changeDivColour(e){
+    e.target.classList.remove("gridWhite");
+    e.target.classList.add("gridBlack");
+}
+
+
+
+for(let i=0;i<16;i++){
+    addGridFile(16);
+}
+
+const divs = document.querySelectorAll("div.gridUnit");
+
+console.log(divs)
+
+divs.forEach((grid) => {
+    grid.addEventListener("mouseover", changeDivColour)
+})
